599ca04e327266dcc166bf53611e427c1d435e3a
Munevera Plojovic <plojovicm3@gmail.com>
2022-09-10 04:02:44 +0200

    expense_items

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import ExpenseItems from "./components/ExpenseItems";
-import "./index.css";
+import React from "react";
+import Expenses from "./components/Expenses/Expenses";
 
 function App() {
   const expenses = [
@@ -23,29 +23,17 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
+  // return React.createElement(
+  //   "div",
+  //   {},
+  //   React.createElement("h2", {}, "Lets get started"),
+  //   React.createElement(Expenses, { items: expenses })
+  // );  - jsx
+
   return (
     <div>
       <h1>Lets get started!</h1>
-      <ExpenseItems
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItems
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-        />
-      <ExpenseItems
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-        />
-      <ExpenseItems
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-        />
+      <Expenses items={expenses}/>
     </div>
   );
 }
--- src/components/Expenses/ExpenseItems.js
+++ src/components/Expenses/ExpenseItems.js
@@ -1,16 +1,18 @@
+import React from "react";
 import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
 import "./ExpenseItems.css"
 
-function ExpenseItems(props) {
+const ExpenseItems=(props)=>{
 
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
       <h2>{props.title}</h2>
       <div className="expense-item__price">${props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
