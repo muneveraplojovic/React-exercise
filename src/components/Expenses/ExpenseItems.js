6f14d4bdb66b310ae8c2f59c47c4d697d5ea63fa
Munevera Plojovic <plojovicm3@gmail.com>
2022-09-17 01:38:02 +0200

    card_imported

--- src/components/Expenses/ExpenseItems.js
+++ src/components/Expenses/ExpenseItems.js
@@ -10,6 +10,7 @@ const ExpenseItems=(props)=>{
       <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
       <h2>{props.title}</h2>
+      <h2>{props.title}</h2>
       <div className="expense-item__price">${props.amount}</div>
       </div>
     </Card>
